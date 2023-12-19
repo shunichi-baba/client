241b280d8e58dbc568be36b28f8e5f91f85af9ec
baba <shunichi_baba@mail.toyota.co.jp>
2023-12-19 10:30:20 +0900

    1st
    Co-authored-by: Satoru-git <satoru-git@users.noreply.github.com>
    Co-authored-by: shunichi-baba <shunichi-baba@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeTabs from "./screens/HomeTabs";
+import UserScreen from "./screens/UserScreen";
+import SettingScreen from "./screens/SettingScreen";
+import LoginScreen from "./screens/LoginScreen";
+import "react-native-gesture-handler";
+import SignupScreen from "./screens/SignupScreen";
+import MainRecipesListScreen from "./screens/MainRecipesListScreen";
+import SideRecipesListScreen from "./screens/SideRecipesListScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer initialRouteName="User">
+      {/* スタック遷移 */}
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeTabs}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen
+          name="MainRecipesList"
+          component={MainRecipesListScreen}
+        />
+        <Stack.Screen
+          name="SideRecipesList"
+          component={SideRecipesListScreen}
+        />
+        <Stack.Screen name="User" component={UserScreen} />
+        <Stack.Screen name="Setting" component={SettingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
